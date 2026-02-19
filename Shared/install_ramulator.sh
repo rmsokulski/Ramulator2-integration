faf8ad2b75cdc7706defb971dc6eedbb2d2653f3
Rodrigo <rodrigosokulski@hotmail.com>
2026-02-18 17:13:49 -0700

    Melhorado

--- Shared/install_ramulator.sh
+++ Shared/install_ramulator.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/CMU-SAFARI/ramulator2 && cd ramulator2 && mkdir build && cd build && cmake .. && make -j 4 && cp ./ramulator2 ../ramulator2 && cd ..
+cp ramulator2/libramulator.so OrCS_Mestrado/ramulator_lib/
 
 echo "Installation completed!"
